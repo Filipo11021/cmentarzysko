e6ed404f00d1c8568d5e9c17209b709f33f0071f
Filipo <filipfilip280@gmail.com>
2024-07-21 22:53:08 +0200

    feat(frontend): create about league section

--- src/app/(app)/(homepage)/_components/about-league.tsx
+++ src/app/(app)/(homepage)/_components/about-league.tsx
@@ -1,14 +1,41 @@
 import { Homepage } from 'payload-types'
 import { FC } from 'react'
+import { Button } from '../../_components/ui/button'
+import { CmsLink } from '../../_components/cms-link'
+import { Container } from '../../_components/ui/container'
 
 type Props = {
   data: Homepage['aboutLeague']
 }
 
-export const AboutLeague: FC<Props> = () => {
+export const AboutLeague: FC<Props> = ({ data }) => {
   return (
-    <section>
-      <h2>AboutLeague</h2>
-    </section>
+    <Container asChild>
+      <section
+        className="flex py-12 flex-wrap justify-between gap-[16rem]"
+        aria-labelledby="about-league"
+      >
+        <div className="flex-1 flex flex-col gap-8">
+          <h2 id="about-league" className='opacity-25 uppercase'>{data.heading}</h2>
+          <h3 className='font-pirateOne text-5xl text-balance'>{data.title}</h3>
+          <p className='opacity-65'>{data.description}</p>
+          <div className='flex flex-wrap'>
+            {data.buttons?.map(({ label, link }, i) =>
+              link ? (
+                <Button key={i} asChild>
+                  <CmsLink data={link}>{label}</CmsLink>
+                </Button>
+              ) : null,
+            )}
+          </div>
+        </div>
+        <div className="flex-1">
+          <video
+            controls
+            src="https://test-videos.co.uk/vids/bigbuckbunny/mp4/h264/360/Big_Buck_Bunny_360_10s_1MB.mp4"
+          ></video>
+        </div>
+      </section>
+    </Container>
   )
 }
