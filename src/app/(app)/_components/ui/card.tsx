15156105a5004ad9719ad9291d0892d537d13d9c
Filipo <filipfilip280@gmail.com>
2024-07-21 21:23:13 +0200

    feat(frontend): create reusable card component

---
+++ src/app/(app)/_components/ui/card.tsx
@@ -0,0 +1,42 @@
+import * as React from 'react'
+
+import { cn } from '@/app/(app)/_utils/cn'
+
+const Card: React.FC<
+  React.HTMLAttributes<HTMLDivElement> & { variant?: 'primary' | 'secondary' }
+> = ({ className, variant = 'primary', ...props }) => (
+  <div
+    className={cn(
+      'rounded-lg border border-[#54619C] bg-[#232335] px-8 py-6',
+      {
+        'text-white': variant === 'primary',
+        'shadow-[0_4px_16px_0px_rgba(190,138,87,0.25)]': variant === 'secondary',
+      },
+      className,
+    )}
+    {...props}
+  />
+)
+
+const CardHeader: React.FC<React.HTMLAttributes<HTMLDivElement>> = ({ className, ...props }) => (
+  <div className={cn('flex flex-col space-y-1.5 p-6', className)} {...props} />
+)
+
+const CardTitle: React.FC<React.HTMLAttributes<HTMLHeadingElement>> = ({ className, ...props }) => (
+  <h3 className={cn('font-semibold leading-none tracking-tight', className)} {...props} />
+)
+
+const CardDescription: React.FC<React.HTMLAttributes<HTMLParagraphElement>> = ({
+  className,
+  ...props
+}) => <p className={cn('text-sm text-muted-foreground', className)} {...props} />
+
+const CardContent: React.FC<React.HTMLAttributes<HTMLDivElement>> = ({ className, ...props }) => (
+  <div className={cn('p-6 pt-0', className)} {...props} />
+)
+
+const CardFooter: React.FC<React.HTMLAttributes<HTMLDivElement>> = ({ className, ...props }) => (
+  <div className={cn('flex items-center p-6 pt-0', className)} {...props} />
+)
+
+export { Card, CardHeader, CardFooter, CardTitle, CardDescription, CardContent }
