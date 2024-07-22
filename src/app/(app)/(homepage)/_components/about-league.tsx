dab6d14ca33db46147b7c13956c88bdadd99c7ea
Filipo <filipfilip280@gmail.com>
2024-07-22 03:47:23 +0200

    feat(frontend): change background of about-league section

--- src/app/(app)/(homepage)/_components/about-league.tsx
+++ src/app/(app)/(homepage)/_components/about-league.tsx
@@ -10,32 +10,34 @@ type Props = {
 
 export const AboutLeague: FC<Props> = ({ data }) => {
   return (
-    <Container asChild>
-      <section
-        className="flex py-12 flex-wrap justify-between gap-[16rem]"
-        aria-labelledby="about-league"
-      >
-        <div className="flex-1 flex flex-col gap-8">
-          <h2 id="about-league" className="opacity-25 uppercase">
-            {data.heading}
-          </h2>
-          <h3 className="font-pirateOne text-5xl text-balance">{data.title}</h3>
-          <p className="opacity-65">{data.description}</p>
-          <div className="flex flex-wrap gap-6">
-            {data.buttons?.map(({ label, link }, i) => (
-              <Button key={i} asChild>
-                <CmsLink data={link}>{label}</CmsLink>
-              </Button>
-            ))}
+    <div className="bg-[rgb(25,25,38)]">
+      <Container asChild>
+        <section
+          className="flex py-32 flex-wrap justify-between gap-[16rem] "
+          aria-labelledby="about-league"
+        >
+          <div className="flex-1 flex flex-col gap-8">
+            <h2 id="about-league" className="opacity-25 uppercase">
+              {data.heading}
+            </h2>
+            <h3 className="font-pirateOne text-5xl text-balance">{data.title}</h3>
+            <p className="opacity-65">{data.description}</p>
+            <div className="flex flex-wrap gap-6">
+              {data.buttons?.map(({ label, link }, i) => (
+                <Button key={i} asChild>
+                  <CmsLink data={link}>{label}</CmsLink>
+                </Button>
+              ))}
+            </div>
           </div>
-        </div>
-        <div className="flex-1">
-          <video
-            controls
-            src="https://test-videos.co.uk/vids/bigbuckbunny/mp4/h264/360/Big_Buck_Bunny_360_10s_1MB.mp4"
-          ></video>
-        </div>
-      </section>
-    </Container>
+          <div className="flex-1">
+            <video
+              controls
+              src="https://test-videos.co.uk/vids/bigbuckbunny/mp4/h264/360/Big_Buck_Bunny_360_10s_1MB.mp4"
+            ></video>
+          </div>
+        </section>
+      </Container>
+    </div>
   )
 }
