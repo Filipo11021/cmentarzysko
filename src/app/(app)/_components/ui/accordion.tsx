2f8abd3560eb479154b4a009a15d733a45ea6703
Filipo <filipfilip280@gmail.com>
2024-07-23 06:29:25 +0200

    feat(frontend): reusable accordion component

---
+++ src/app/(app)/_components/ui/accordion.tsx
@@ -0,0 +1,54 @@
+'use client'
+
+import * as React from 'react'
+import * as AccordionPrimitive from '@radix-ui/react-accordion'
+import { ChevronDownIcon } from 'lucide-react'
+import { cn } from '@/app/(app)/_utils/cn'
+
+const Accordion = AccordionPrimitive.Root
+
+const AccordionItem: React.FC<React.ComponentProps<typeof AccordionPrimitive.Item>> = ({
+  className,
+  ...props
+}) => (
+  <AccordionPrimitive.Item
+    className={cn('border px-8 py-6 bg-[#232335] border-[#54619C] rounded-lg', className)}
+    {...props}
+  />
+)
+
+const AccordionTrigger: React.FC<React.ComponentProps<typeof AccordionPrimitive.Trigger>> = ({
+  className,
+  children,
+  ...props
+}) => (
+  <AccordionPrimitive.Header className="flex">
+    <AccordionPrimitive.Trigger
+      className={cn(
+        'flex flex-1 mb-3 items-center justify-between text-3xl font-bold transition-all hover:underline [&[data-state=open]>svg]:rotate-180',
+        className,
+      )}
+      {...props}
+    >
+      {children}
+      <ChevronDownIcon className="h-4 w-4 mr-6 shrink-0 text-muted-foreground transition-transform duration-200" />
+    </AccordionPrimitive.Trigger>
+  </AccordionPrimitive.Header>
+)
+AccordionTrigger.displayName = AccordionPrimitive.Trigger.displayName
+
+const AccordionContent = React.forwardRef<
+  React.ElementRef<typeof AccordionPrimitive.Content>,
+  React.ComponentPropsWithoutRef<typeof AccordionPrimitive.Content>
+>(({ className, children, ...props }, ref) => (
+  <AccordionPrimitive.Content
+    ref={ref}
+    className="overflow-hidden opacity-65 data-[state=closed]:animate-accordion-up data-[state=open]:animate-accordion-down"
+    {...props}
+  >
+    <div className={cn('pt-0', className)}>{children}</div>
+  </AccordionPrimitive.Content>
+))
+AccordionContent.displayName = AccordionPrimitive.Content.displayName
+
+export { Accordion, AccordionItem, AccordionTrigger, AccordionContent }
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -20,6 +20,20 @@ module.exports = {
         gradient:
           'linear-gradient(96deg, rgba(29, 29, 45, 0.25) 0%, rgba(216, 156, 99, 0.25) 97.83%), #232335',
       },
+      keyframes: {
+        'accordion-down': {
+          from: { height: '0' },
+          to: { height: 'var(--radix-accordion-content-height)' },
+        },
+        'accordion-up': {
+          from: { height: 'var(--radix-accordion-content-height)' },
+          to: { height: '0' },
+        },
+      },
+      animation: {
+        'accordion-down': 'accordion-down 0.2s ease-out',
+        'accordion-up': 'accordion-up 0.2s ease-out',
+      },
     },
   },
   plugins: [],
