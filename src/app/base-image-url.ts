bee9692ed811bbf4c909bfa02a68798427198dfe
Filipo <filipfilip280@gmail.com>
2024-07-23 15:43:39 +0200

    fix: add htpps schema to vercel production url

--- src/app/base-image-url.ts
+++ src/app/base-image-url.ts
@@ -1,1 +1,2 @@
-export const baseCmsMediaUrl = process.env.VERCEL_URL ?? 'http://localhost:3000'
+export const baseCmsMediaUrl =
+  `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}` ?? 'http://localhost:3000'
