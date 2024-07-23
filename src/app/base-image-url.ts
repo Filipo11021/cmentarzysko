4be446ad7d29f111ac7730467a7c751ec4bf59b1
Filipo <filipfilip280@gmail.com>
2024-07-23 16:04:22 +0200

    fix: set conditional vercel url

--- src/app/base-image-url.ts
+++ src/app/base-image-url.ts
@@ -1,2 +1,3 @@
-export const baseCmsMediaUrl =
-  `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}` ?? 'http://localhost:3000'
+export const baseCmsMediaUrl = process.env.VERCEL_PROJECT_PRODUCTION_URL
+  ? `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`
+  : 'http://localhost:3000'
