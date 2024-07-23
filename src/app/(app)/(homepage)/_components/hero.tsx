19067835f04f3f08adbc0f54bef7b8a61efdf6f9
Filipo <filipfilip280@gmail.com>
2024-07-23 16:39:52 +0200

    feat: hero animation and remove secondary variant from buttons

--- src/app/(app)/(homepage)/_components/hero.tsx
+++ src/app/(app)/(homepage)/_components/hero.tsx
@@ -13,41 +13,48 @@ type Props = {
 
 export const Hero: FC<Props> = ({ data }) => {
   return (
-    <Container asChild>
-      <section aria-labelledby="hero" className="relative py-32">
-        <div className="absolute right-0"></div>
-        <div className="flex flex-col gap-12 max-w-[50rem] mx-auto">
-          <div className="text-center">
-            <h1 id="hero" className="mb-6 text-5xl md:text-7xl font-pirateOne text-balance">
-              {data.title}
-            </h1>
-            <p className="leading-6">{data.description}</p>
-          </div>
-          <div className="flex flex-col items-center">
-            <h2 className="mb-6 text-center">{data.timerTitle}</h2>
-            <Countdown />
-          </div>
-          {data.buttons?.length ? (
-            <div className="flex flex-col md:flex-row gap-6 lg:gap-12 justify-center">
-              {data.buttons.map(({ label, link }, i) => (
-                <Button
-                  key={i}
-                  asChild
-                  className="flex-1 md:flex-initial"
-                  variant={i === 0 ? 'secondary' : 'primary'}
-                >
-                  <CmsLink data={link}>{label}</CmsLink>
-                </Button>
-              ))}
+    <section aria-labelledby="hero" className="relative py-32">
+      <div className="flex justify-center overflow-hidden">
+        <video
+          style={{ transform: 'scale(-1, 1)' }}
+          className="hidden xl:block"
+          autoPlay
+          controls={false}
+          loop
+          src="/ship.webm"
+        />
+
+        <div>
+          <div className="flex flex-col pb-20 gap-12 w-full lg:w-[50rem] max-w-[50rem] mx-auto">
+            <div className="text-center">
+              <h1 id="hero" className="mb-6 text-5xl md:text-7xl font-pirateOne text-balance">
+                {data.title}
+              </h1>
+              <p className="leading-6">{data.description}</p>
             </div>
-          ) : null}
+            <div className="flex flex-col items-center">
+              <h2 className="mb-6 text-center">{data.timerTitle}</h2>
+              <Countdown />
+            </div>
+            {data.buttons?.length ? (
+              <div className="flex flex-col md:flex-row gap-6 lg:gap-12 justify-center">
+                {data.buttons.map(({ label, link }, i) => (
+                  <Button key={i} asChild className="flex-1 md:flex-initial">
+                    <CmsLink data={link}>{label}</CmsLink>
+                  </Button>
+                ))}
+              </div>
+            ) : null}
+          </div>
         </div>
-        <div className="flex gap-6 mt-20 flex-col items-center">
-          <h2 className="opacity-70">GŁÓWNY PARTNER</h2>
-          <Image alt="" width={272} height={50} src="/endorfy.png" />
-        </div>
-        <div className="absolute left-0"></div>
-      </section>
-    </Container>
+
+        <video className="hidden xl:block" autoPlay controls={false} loop src="/ship.webm" />
+      </div>
+
+      <div className="flex gap-6 flex-col items-center">
+        <h2 className="opacity-70">GŁÓWNY PARTNER</h2>
+        <Image alt="" width={272} height={50} src="/endorfy.png" />
+      </div>
+    </section>
   )
 }
