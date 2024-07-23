aa44a3879bb8cb21935a5ae7d929a1d635be7548
Filipo <filipfilip280@gmail.com>
2024-07-23 16:38:49 +0200

    fix: remove shadow on first countdown element

--- src/app/(app)/(homepage)/_components/countdown.tsx
+++ src/app/(app)/(homepage)/_components/countdown.tsx
@@ -4,24 +4,13 @@ import { ReactNode } from 'react'
 import { Card } from '../../_components/ui/card'
 import { useCountdown } from '../../_utils/use-countdown'
 
-function CountdownCard({
-  name,
-  children,
-  variant = 'primary',
-}: {
-  children: ReactNode
-  name: string
-  variant?: 'primary' | 'secondary'
-}) {
+function CountdownCard({ name, children }: { children: ReactNode; name: string }) {
   return (
     <div className="flex flex-col items-center gap-3">
-      <Card
-        className="flex px-5 py-3 flex-col justify-center items-center text-2xl font-bold h-14 w-14 lg:h-[4.7rem] lg:w-[4.7rem]"
-        variant={variant}
-      >
+      <Card className="flex px-5 py-3 flex-col justify-center items-center text-2xl font-bold h-14 w-14 lg:h-[4.7rem] lg:w-[4.7rem]">
         {children}
       </Card>
-      <p className='opacity-70 uppercase'>{name}</p>
+      <p className="opacity-70 uppercase">{name}</p>
     </div>
   )
 }
@@ -30,9 +19,7 @@ export function Countdown() {
   const { seconds, minutes, hours, days } = useCountdown(new Date('2024-07-26T20:00:00+0000'))
   return (
     <div className="flex gap-8">
-      <CountdownCard variant="secondary" name="Dni">
-        {days}
-      </CountdownCard>
+      <CountdownCard name="Dni">{days}</CountdownCard>
       <CountdownCard name="Godzin">{hours}</CountdownCard>
       <CountdownCard name="Minut">{minutes}</CountdownCard>
       <CountdownCard name="Sekund">{seconds}</CountdownCard>
